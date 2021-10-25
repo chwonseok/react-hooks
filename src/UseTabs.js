6812c0944c6f3ec8843f930eb15622d3a2843739
cws0316 <dev.chwonseok@gmail.com>
2021-10-25 19:02:23 +0900

    Modify: little change on content of UseTabJs

--- src/UseTabs.js
+++ src/UseTabs.js
@@ -9,10 +9,19 @@ export const content = [
     tab: 'Section 2',
     content: 'This is contect of the Section 2',
   },
+  {
+    tab: 'Section 3',
+    content: 'This is contect of the Section 3',
+  },
+  {
+    tab: 'Section 4',
+    content: 'This is contect of the Section 4',
+  },
 ];
 
 export const useTabs = (initialTab, allTabs) => {
   const [curIndex, setCurIndex] = useState(initialTab);
+
   if (!allTabs || !Array.isArray(allTabs)) return;
 
   return {
