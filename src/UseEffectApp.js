5ae12806eb147940340d99955869e07aded53648
cws0316 <dev.chwonseok@gmail.com>
2021-10-29 17:48:06 +0900

    Add: custom hook called useTitle - it has useEffect inside to change the title

--- src/UseEffectApp.js
+++ src/UseEffectApp.js
@@ -1,8 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
+  };
+
+  useEffect(updateTitle, [title]);
+
+  return setTitle;
+};
 
 export default function UseEffectApp() {
+  const titleUpdater = useTitle('Loading...');
+  setTimeout(() => titleUpdater('Home'), 3000);
+  return (
+    <div>
+      <div>Hey</div>
+    </div>
+  );
+}
+
+/* useEffect에 대한 기본
+  export default function UseEffectApp() {
   const [number, setNumber] = useState(0);
   const [aNumber, setANumber] = useState(0);
+
+  const sayHello = () => console.log('Hello');
+  useEffect(sayHello, [number]);
 
   return (
     <div>
@@ -11,4 +37,4 @@ export default function UseEffectApp() {
       <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
-}
+} */
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import UseStateApp from './UseStateApp';
+// import UseStateApp from './UseStateApp';
 import UseEffectApp from './UseEffectApp';
 
 ReactDOM.render(
