5b701d9057d40e3052da90368c00bbf9e6536097
cws0316 <dev.chwonseok@gmail.com>
2021-11-01 17:44:41 +0900

    Modify: changed useState to useReducer

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  if (action.type === 'INCREMENT')
+    return { count: state.count + 1, showText: state.showText };
+  if (action.type === 'toggleShowText')
+    return { count: state.count, showText: !state.showText };
+  else return state;
+};
 
 const UseReducer = () => {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(true);
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
+
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button
         onClick={() => {
-          setCount(count + 1);
-          setShowText(!showText);
+          dispatch({ type: 'INCREMENT' });
+          dispatch({ type: 'toggleShowText' });
         }}
       >
         Click
       </button>
-      {showText && <p>This is a text</p>}
+      {state.showText && <p>This is a text</p>}
     </div>
   );
 };
